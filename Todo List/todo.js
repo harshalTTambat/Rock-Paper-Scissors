f9f7faf13a117ee9f4b97f4cf68a665d9d2725df
Harshal <harshaltambat38@gmail.com>
2023-10-23 23:33:33 +0530

    todo almost done

--- Todo List/todo.js
+++ Todo List/todo.js
@@ -1,16 +1,36 @@
 
-const todoList = [];
+const todoList = [{
+    name:"fuck",
+    dueDate: "2023-13-10"
+},
+{
+    name:"fuck again",
+    dueDate: "2023-13-11"
+}];
 
-//renderTodoList();
+renderTodoList();
 
+// this technic  use to generate html code without manually type it 
 function renderTodoList()
 {
-    let todoListHTML = '';
-
+    let todoListHTML = ''; 
     for (let i=0; i<todoList.length; i++)
     {
-        const todo = todoList[i];
-        const html = `<p>${todo}</p>`;
+        const todoObject = todoList[i];
+        // const name = todoObject.name;
+        // const dueDate = todoObject.dueDate;
+        const {name, dueDate} = todoObject;
+        const html = `
+                    <div>${name}</div>
+                    <div>${dueDate}</div>
+                    <button
+                        onclick = "
+                            todoList.splice(${i}, 1);
+                            renderTodoList();
+                        " class="delete-todo-button">Delete
+                    </button>  
+                `;
+        // .splice is used to delete on specified index and specified number of values
         todoListHTML += html;
         document.querySelector('.js-todoList')
             .innerHTML = todoListHTML;
@@ -23,8 +43,20 @@ function addToDo()
     const inputEle = document.querySelector('.js-name-input');
     const name = inputEle.value;
 
-    todoList.push(name);
+    const inputDate = document.querySelector('.js-todo-date');
+    const dueDate = inputDate.value;
+
+    todoList.push({
+        //name: name, 
+        //dueDate: dueDate
+        // when property and value names are same 
+        //  then we can write it onece
+        name,
+        dueDate
+
+    });
 
     inputEle.value = '';
+    
     renderTodoList();
 }
